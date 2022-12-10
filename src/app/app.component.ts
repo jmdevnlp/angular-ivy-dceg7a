699ce34b90a033e793deaada07bb78610e711914
jmdevnlp <79724198+jmdevnlp@users.noreply.github.com>
2022-12-09 19:10:06 -0500

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,5 +8,7 @@ import { Component, VERSION } from '@angular/core';
 export class AppComponent {
   name = 'Angular ' + VERSION.major;
   date = "Today's date is " + String(Date());
-  rows = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+  rows = [
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+  ];
 }
